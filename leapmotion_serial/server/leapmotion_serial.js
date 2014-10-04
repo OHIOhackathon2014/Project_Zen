4f4a8a54a0e21ec90b2f77d6711b4805234d0300
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2014-10-04 08:54:55 -0700

    Added bridge between client and serial

--- leapmotion_serial/server/leapmotion_serial.js
+++ leapmotion_serial/server/leapmotion_serial.js
@@ -1,6 +1,14 @@
+function getDValue(velocity) {
+	var d;
+	if (velocity === 0) d = 2;
+	else if (velocity < 0) d = 1;
+	else d = 0;
+	return d;
+}
+
 Meteor.startup(function () {
 	var SerialPort = serialport.SerialPort;
-	/*var arduino = new SerialPort("/dev/ttyACM0", {
+	var arduino = new SerialPort("/dev/ttyACM1", {
 		baudrate: 9600
 	});
 	arduino.open(function(err) {
@@ -8,7 +16,14 @@ Meteor.startup(function () {
 			console.log("Failed to open: " + err);
 		} else {
 			console.log("Serial port open");
-			arduino.write("Hello World!");
+			Meteor.methods({
+				writeData: function(velocity, y) {
+					var firstByte = 8 * y + 4 * (velocity === 0 ? 1 : 0) + getDValue(velocity);
+					var secondByte = Math.abs(velocity);
+					console.log(firstByte + " " + secondByte);
+					arduino.write(String.fromCharCode(firstByte) + String.fromCharCode(secondByte));
+				}
+			});
 		}	
-	});*/
+	});
 });
